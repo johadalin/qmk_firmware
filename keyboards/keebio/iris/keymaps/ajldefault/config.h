424dc8fe3f7c03701760f5b95441936b2a91f62f
Adam Lindley <johadalin@hotmail.com>
2023-04-13 23:42:20 +0100

    switch to right master, as left is dead

--- keyboards/keebio/iris/keymaps/ajldefault/config.h
+++ keyboards/keebio/iris/keymaps/ajldefault/config.h
@@ -18,5 +18,9 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #pragma once
 
 // #define USE_I2C
-#define EE_HANDS
+//#define EE_HANDS
 #define SPLIT_USB_DETECT
+// AJL TEST
+//#define MASTER_LEFT
+#define MASTER_RIGHT
+
