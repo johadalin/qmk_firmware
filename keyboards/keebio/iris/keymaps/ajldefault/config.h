52a8f219f9228b9782e8f139b115360d59e779ec
Adam Lindley <johadalin@hotmail.com>
2023-08-07 16:41:25 +0100

    Revert "switch to right master, as left is dead"
    This reverts commit 424dc8fe3f7c03701760f5b95441936b2a91f62f.

--- keyboards/keebio/iris/keymaps/ajldefault/config.h
+++ keyboards/keebio/iris/keymaps/ajldefault/config.h
@@ -18,9 +18,5 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #pragma once
 
 // #define USE_I2C
-//#define EE_HANDS
+#define EE_HANDS
 #define SPLIT_USB_DETECT
-// AJL TEST
-//#define MASTER_LEFT
-#define MASTER_RIGHT
-
